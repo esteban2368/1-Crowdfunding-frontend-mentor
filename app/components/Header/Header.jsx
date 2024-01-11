fa8b1c245f17a993c659c6347c51cad226c398c8
Esteban DEV <metalbrayan1968@hotmail.com>
2024-01-10 23:39:32 -0500

    Create hook useResponsive and start UI component Header in mobile screen

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,5 +1,7 @@
+'use client'
 import Link from "next/link";
 import Image from "next/image";
+import useResponsive from "@/app/hooks/useResponsive";
 import StyleHeader from './Header.module.css'
 
 import heroDesktop from '../../../public/images/image-hero-desktop.jpg'
@@ -7,24 +9,37 @@ import heroMobile from '../../../public/images/image-hero-mobile.jpg'
 import logo from '../../../public/images/logo.svg'
 
 const Header = () => {
+    const isMobile = useResponsive(375)
     return (
-        <header>
-            <div>
-                <Link href="#">
-                    <Image
-                        src={logo}
-                        alt="Crowdfund logo"
-                    />
-                </Link>
+        <header className={`${StyleHeader.header} py-7 px-6`}>
+            <Image
+                className={StyleHeader.header__background}
+                src={isMobile ? heroMobile : heroDesktop}
+                priority
+                alt=""
+            />
+            <div className={StyleHeader.header__menu}>
+                <div>
+                    <Link href="#">
+                        <Image
+                            src={logo}
+                            alt="Crowdfund logo"
+                        />
+                    </Link>
+                </div>
+                {!isMobile && (
+                    <nav>
+                        <ul>
+                            <li><Link href="#">About</Link></li>
+                            <li><Link href="#">Discover</Link></li>
+                            <li><Link href="#">Get started</Link></li>
+                        </ul>
+                    </nav>
+                )} 
+                {isMobile && (
+                    <p>mobile</p>
+                )}
             </div>
-            <nav>
-                <ul>
-                    <li><Link href="#">About</Link></li>
-                    <li><Link href="#">Discover</Link></li>
-                    <li><Link href="#">Get started</Link></li>
-                </ul>
-            </nav>  
-            
         </header>
     )
 }
--- app/hooks/useResponsive.js
+++ app/hooks/useResponsive.js
@@ -1,1 +1,24 @@
+'use client'
+import { useState, useEffect, useCallback } from "react"
 
+const useResponsive = (sizeScreen) =>{
+    const [isMobile, setIsMobile] = useState(!window.matchMedia(`(min-width: ${sizeScreen}px)`).matches)
+
+    const handleScreenSize = useCallback(e =>{
+        setIsMobile(!e.matches)
+    },[])
+
+    useEffect(()=>{
+        const screen = window.matchMedia(`(min-width: ${sizeScreen}px)`)
+        handleScreenSize(screen)
+        screen.addEventListener("change", handleScreenSize)
+
+        return () => {
+            screen.removeEventListener("change", handleScreenSize)
+        }
+    },[sizeScreen])
+
+    return isMobile
+}
+
+export default useResponsive
